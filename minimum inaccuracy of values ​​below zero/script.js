a96e731a13fefe29de89243706ffc863e1137210
Rafael Botelho Cabral <118222979+rafaelbcabral@users.noreply.github.com>
2023-03-27 16:27:45 -0300

    att of course

---
+++ minimum inaccuracy of values ​​below zero/script.js
@@ -0,0 +1,14 @@
+let num1 = 1; // number
+
+console.log(num1.toString()); /* esse number vira uma string por conta do toString() 
+E dentro desse parenteses () qualquer valor que botar ficaria a representacao binaria */
+
+console.log(num1.toFixed(2)); // para ter um maximo de numero apos a virgula
+console.log(number.isInteger(num1)); // para ver se o num é inteiro
+
+num += num2; // num1 = num1 + num2;
+
+num1 = parseFloat(num1.toFixed(2)); // para colocar um número como 0,99999 para o modo inteiro, ou seja, 1.
+
+
+

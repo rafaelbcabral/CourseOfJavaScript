2d11249f2512cb22a14669db15aa39f5066cfa70
Rafael Botelho Cabral <rafael1311cabral@gmail.com>
2024-05-31 11:19:31 -0300

    removing

--- ECMAScript/index.js
+++
@@ -1,6 +0,0 @@
-// ECMAScript or JavaScript
-
-nome = 'Luiz'; // não declarar uma variavel sem let, const ou var.
-let nome = 'Otávio';
-
-console.log(nome);
--- functions/script.js
+++
@@ -1,7 +0,0 @@
-function soma(x = 1, y = 1) { // irá atribuir 1 e 1 caso não seja declarado nada posteriormente.
-    const resultado = x + y;
-    return resultado; // a partir do return nada mais é executado na função.
-}
-
-const resultado = soma(4, 2); // atribui os valores caso estes mesmos não tenham.
-console.log(resultado); //executa o resultado no console.

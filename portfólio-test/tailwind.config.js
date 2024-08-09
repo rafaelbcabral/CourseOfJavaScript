192a18b9f846c07ce92d4965f73a3c9e3666c74f
Rafael Botelho Cabral <rafael1311cabral@gmail.com>
2024-08-09 17:50:39 -0300

    constructing..

--- portfólio-test/tailwind.config.js
+++ portfólio-test/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4caf50',
+      },
+    },
   },
   plugins: [],
 }
